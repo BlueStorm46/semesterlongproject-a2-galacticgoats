0de9df94b20310e6abdb2dc0cb9a214e332be42d
Dianna Radpour <diannara@buffalo.edu>
2017-02-25 14:24:32 -0500

    bs tests

---
+++ src/edu/buffalo/cse116/BurningShipTest.java
@@ -0,0 +1,54 @@
+package edu.buffalo.cse116;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class BurningShipTest {
+
+	@Test
+	public void test() {
+		BurningShip burningSet = new BurningShip();
+		burningSet.escapetime(-1.7443359374999874, -0.017451171875000338);
+		System.out.println(burningSet.escapetime(-1.7443359374999874, -0.017451171875000338));
+		assertEquals(4, burningSet.escapetime(-1.7443359374999874, -0.017451171875000338), 0);
+	}
+
+	@Test
+	public void testescapefor0() {
+		BurningShip burningSet = new BurningShip();
+		burningSet.escapetime(-2, 2);
+		System.out.println(burningSet.escapetime(-2, 2));
+		assertEquals(0, burningSet.escapetime(-2, 2), 0);
+
+	}
+
+	@Test
+	public void testescapefor1() {
+		BurningShip burningSet = new BurningShip();
+		burningSet.escapetime(-2, 2);
+		System.out.println(burningSet.escapetime(-2, 2));
+		assertEquals(1, burningSet.escapetime(-2, 2), 1);
+	}
+
+	@Test
+	public void testXaxis() {
+		BurningShip burningSet = new BurningShip();
+		assertEquals(0, burningSet.createBS()[372][0], 0);
+	}
+
+	@Test
+	public void testyaxis() {
+		BurningShip burningSet = new BurningShip();
+		assertEquals(0, burningSet.createBS()[0][393], 0);
+	}
+
+	@Test
+	public void fractalSizeTest() {
+		BurningShip burningSet = new BurningShip();
+		assertEquals(512, burningSet.createBS()[0].length, 0);
+		assertEquals(512, burningSet.createBS().length, 0);
+	}
+
+}
