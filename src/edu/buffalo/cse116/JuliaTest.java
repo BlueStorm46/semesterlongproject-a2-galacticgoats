8daba9a37e0cfb3db55cde429c4745812c31016b
Dianna Radpour <diannara@buffalo.edu>
2017-02-25 11:06:51 -0500

    Add files via upload
    julia test

---
+++ src/edu/buffalo/cse116/JuliaTest.java
@@ -0,0 +1,45 @@
+package edu.buffalo.cse116;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class JuliaTest {
+
+	@Test
+	public void test() {
+		Julia juliaSet = new Julia();
+		juliaSet.escapetime(1.0492187499999897, -0.234375);
+		System.out.println(juliaSet.escapetime(1.0492187499999897, -0.234375));
+		assertEquals(4, juliaSet.escapetime(1.0492187499999897, -0.234375), 0);
+	}
+
+	@Test
+	public void test2() {
+		Julia juliaSet = new Julia();
+		juliaSet.escapetime(1.6933593749999853, 0.9765625);
+		System.out.println(juliaSet.escapetime(1.6933593749999853, 0.9765625));
+		assertEquals(255, juliaSet.escapetime(1.6933593749999853, 0.9765625), 0);
+	}
+
+	@Test
+	public void testXaxis() {
+		Julia juliaSet = new Julia();
+		assertEquals(0, juliaSet.createJulia()[372][0], 0);
+	}
+
+	@Test
+	public void testyaxis() {
+		Julia juliaSet = new Julia();
+		assertEquals(0, juliaSet.createJulia()[0][393], 0);
+	}
+
+	@Test
+	public void fractalSizeTest() {
+		Julia juliaSet = new Julia();
+		assertEquals(512, juliaSet.createJulia()[0].length, 0);
+		assertEquals(512, juliaSet.createJulia().length, 0);
+	}
+
+}
